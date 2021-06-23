ffb7de6048e30968dcd383aeb80d7aab68ef3c96
Daksh patel <helloitsdaksh007@gmail.com>
2021-06-23 15:16:53 +0530

    final

--- API.py
+++ API.py
@@ -1,8 +1,11 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify
 import base64
-from main import *
-app = Flask(__name__)
+from keras.models import load_model
+import cv2
+import numpy as np
 
+model = load_model("Deploy-Digit-Recognizer/ model.h5")
+app = Flask(__name__)
 def convertImage(imgData1):
 	with open('Deploy-Digit-Recognizer/images/output.jpg','wb') as output:
 		output.write(base64.b64decode(imgData1["base64"]))
@@ -13,8 +16,18 @@ def index():
 
 @app.route('/predict/',methods=['GET','POST'])
 def predict():
-	imgData = request.get_json()
-	convertImage(imgData)
-	print ("debug")	
+	print("debug")
+	image_path = "Deploy-Digit-Recognizer/images/output.jpg"
+	img_array = cv2.imread(image_path)
+	new_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+	new_array = np.invert(new_array)
+	new_array = cv2.resize(new_array, (28,28))
+	new_array = new_array.reshape(1,28,28,1)
+	y_pred = np.argmax(model.predict(new_array), axis=-1)
+	print(y_pred)
+	y_pred= y_pred.tolist()	
+	return jsonify({"result" : y_pred})
+
+if __name__ == "__main__":
+	app.run(debug = True)
 	
-	return jsonify({"result" : init()})
